861a97cb865fd831db6e308c0f8c871b7998831d
Grace-Barton <2getgb@gmail.com>
2022-04-06 13:49:46 -0700

    Landing Page Recommended Stays Update

--- ReactPackage/src/components/TypesOfStays.js
+++ ReactPackage/src/components/TypesOfStays.js
@@ -5,7 +5,18 @@ import './TypesOfStays.css'
 function TypesOfStays(){
     return(
         <div id="types">
-              <h1>Choose Your Type of Stay</h1>  
+              <h1>Choose Your Type of Stay</h1> 
+              <div id ="outer-div">
+                  <div id="circle1"></div>
+                  <div className = "inner-div">
+                      <div className="stay"><img src= { require("./beach-stay.jpg") } /></div>
+                      <div className="stay"><img src ={ require("./city.jpg") }  /></div>
+                      <div className="stay"><img src={ require("./beach-stay.jpg") } /></div>
+                      <div className="stay"><img src= { require("./boat.jpg") } /></div>
+                      <div className="stay"><img src= { require("./country.jpg") } /></div>
+                   </div> 
+                   <div id="circle1"></div>
+              </div>
         </div>
     );
 }
