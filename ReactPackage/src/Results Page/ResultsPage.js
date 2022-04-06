255df58a766eee0c12272723684a4374db1a9403
Rishab Jain <rishab838@gmail.com>
2022-04-06 14:09:40 -0400

    resultpage working again

--- ReactPackage/src/Results Page/ResultsPage.js
+++ ReactPackage/src/Results Page/ResultsPage.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './ResultsPage.css'
-
+import Topbar from './Topbar'
+import Sidebar from './Sidebar'
+import Result from './Result'
 
 
 function ResultsPage(){
     return(
         <div id="resultpage">
-              
+        <Topbar />
+        <div id = "section-one">
+        <Sidebar />
+        <Result />
+        </div>
         </div>
     );
 }
