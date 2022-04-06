d76665e409f598c325b19e5ad061cc6dbc6371e0
Rishab Jain <rishab838@gmail.com>
2022-04-06 03:02:07 -0400

    First draft design of results page

--- ReactPackage/src/Results Page/ResultsPage.js
+++ ReactPackage/src/Results Page/ResultsPage.js
@@ -1,2 +1,17 @@
 import React from "react";
 import './ResultsPage.css'
+import TopBar from './Topbar'
+import SideBar from './Sidebar'
+import Result from './Result'
+
+function ResultsPage(){
+    return(
+        <div id="resultspage">
+        <TopBar />
+        <SideBar />
+        <Result />
+        </div>
+    );
+}
+
+export default ResultsPage;
