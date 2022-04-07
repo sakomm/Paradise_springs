d3d46790e65e098e831a7fb57bef98e3be226215
Rishab Jain <rishab838@gmail.com>
2022-04-07 19:11:52 -0400

    I put things inside the boxes after 7 hours of looking at wrong solutions

--- ReactPackage/src/Results Page/Sidebar.js
+++ ReactPackage/src/Results Page/Sidebar.js
@@ -6,7 +6,43 @@ import './Sidebar.css'
 function Sidebar(){
     return(
         <div id="sidebar">
-              
+        <h1>Filters </h1>
+        <form>
+            <div />
+            <label>
+                One Person
+                <input type="checkbox" name="oneperson" />
+            </label>
+            <div />
+            <label>
+                Two Person
+                <input type="checkbox" name="twoperson" />
+            </label>
+            <div />
+            <label>
+                Three Person
+                <input type="checkbox" name="Threeperson" />
+            </label>
+            <div />
+            <label>
+                Beachfront
+                <input type="checkbox" name="beachfront" />
+            </label>
+            <div />
+            <label>
+                City
+                <input type="checkbox" name="city" />
+            </label>
+            <div />
+            <label>
+                Price
+                <input type="range" min="$1" max="$100" class="slider" id="pricerange" />
+            </label>
+            <div />
+            <label>
+                <input type="submit" id="submit" />
+            </label>
+        </form>
         </div>
     );
 }
