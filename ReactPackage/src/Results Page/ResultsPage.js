ab572a45047fc232e0e346f176bf4e1d1bdc7a9b
Rishab Jain <rishab838@gmail.com>
2022-04-07 04:40:36 -0400

    form prototype set it just isn't in the boxes

--- ReactPackage/src/Results Page/ResultsPage.js
+++ ReactPackage/src/Results Page/ResultsPage.js
@@ -11,6 +11,38 @@ function ResultsPage(){
         <Topbar />
         <div id = "section-one">
         <Sidebar />
+        <form>
+            <div />
+            <label>
+                One Person
+                <input type="checkbox" name="oneperson" />
+            </label>
+            <div />
+            <label>
+                Two Person
+                <input type="checkbox" name="twoperson" />
+            </label>
+            <div />
+            <label>
+                Three Person
+                <input type="checkbox" name="Threeperson" />
+            </label>
+            <div />
+            <label>
+                Beachfront
+                <input type="checkbox" name="beachfront" />
+            </label>
+            <div />
+            <label>
+                City
+                <input type="checkbox" name="city" />
+            </label>
+            <div />
+            <label>
+                Price
+                <input type="range" min="$1" max="$100" class="slider" id="pricerange" />
+            </label>
+        </form>
         <Result />
         </div>
         </div>
