2645ff5a360773d557dc63a450f1eea769dc5fd1
Grace-Barton <2getgb@gmail.com>
2022-04-07 20:06:06 -0700

    new chnages

--- ReactPackage/src/components/Rental.js
+++ ReactPackage/src/components/Rental.js
@@ -7,6 +7,7 @@ import './Rental.css'
 function Rental({description, price, photo}){
     return(
         <div id = "post">
+            <a></a>
             <img class="rental-pic" src= {photo}></img>  
             <p id="property" >Entire residential home in Charlottesville</p>
             <p>Safety Rating:</p>
