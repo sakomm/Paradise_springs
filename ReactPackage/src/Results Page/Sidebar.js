3f53f34efafa1a62f4ace91d87addfcf8e5027f6
sai <sakommuri@gmail.com>
2022-04-23 19:30:37 -0400

    css is pain

--- ReactPackage/src/Results Page/Sidebar.js
+++ ReactPackage/src/Results Page/Sidebar.js
@@ -4,37 +4,48 @@ import './Sidebar.css'
 
 // https://stackoverflow.com/questions/24369197/how-to-add-static-text-inside-an-input-form
 
-function Sidebar(){
-        return(
+function Sidebar() {
+    // Code for Platform selector inspired by https://web.dev/building-a-multi-select-component/ article by Adam Argyle 
+    return (
 
-            <div id="sidebar">
-                <h1 id="sidebar-title">Search Filters</h1>
-                    <form>
-                        <div id="bed-inputs">
-                            <input id="input-bed" type="number" min="1" max="10" placeholder="Number of Beds" />
-                        </div>
-                        <div id="price-inputs">
-                            <input id="sidebar-input" type="number" name="price" min="0" max="5000" step="100" placeholder='Price Range'/>
-                        </div>
-                        <div id="saftey-inputs">
-                            <input id="sidebar-input" type="number" name="Saftey" min="0" max="100" step="5" placeholder='Saftey Filter'/>
-                        </div>
-                        <div id="rating-inputs">
-                            <input id="sidebar-input" type="number" name="rating" min="0" max="5" step="1" placeholder='Rating' />
-                        </div>
-                        <div id = "platform-selector">
-                            <select id="platform-select" multiple> 
-                                <option value="AirBnB">AirBnB</option>
-                                <option value="Booking">Booking.com</option>
-                                <option value="VRBO">VRBO</option>
-                                <option value="Hotels">Hotels.com</option>
-                            </select>    
-                        </div>
+        <div id="sidebar">
+            <h1 id="sidebar-title">Search Filters</h1>
+            <form>
+                <label for="nbeds"> Number of Bedrooms: </label>
+                <input aria-label='nbeds' id="sidebar-input" type="number" min="1" max="10" placeholder="Number of Beds" />
+                <label for="price"> Price Max: </label>
+                <input aria-lable='price' id="sidebar-input" type="number" name="price" min="0" max="5000" step="100" placeholder='Price Range' />
+                <label for="saftey"> Safety Min: </label>
+                <input aria-label='saftey' id="sidebar-input" type="number" name="Saftey" min="0" max="100" step="5" placeholder='Saftey Filter' />
+                <label for="ratings"> Rating Min: </label>
+                <input aria-lable='rating' id="sidebar-input" type="number" name="rating" min="0" max="5" step="1" placeholder='Rating' />
 
-                    </form>
-            </div>
-        );
-    }
+                <fieldset>
+                    <legend>Platform Selector</legend>
+                    <div>
+                        <input type="checkbox" id="airbnb" name="platforms" value="airbnb" />
+                        <label for="airbnb">AirBnB</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hotels" name="platforms" value="hotels" />
+                        <label for="hotels">Hotels.com</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="vrbo" name="platforms" value="vrbo" />
+                        <label for="vrbo">VRBO</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="booking" name="platforms" value="booking" />
+                        <label for="booking">Booking.com</label>
+                    </div>
+                </fieldset>
+
+            </form>
+        </div>
+
     
+    );
+}
+
 
 export default Sidebar;
